7d47a2bedc0a5a5751ea26619117d9b8eb48908f
bgupta7 <69441804+bgupta7@users.noreply.github.com>
2020-08-09 22:01:42 -0500

    Add files via upload
    initial commit

---
+++ dist/js/main.js
@@ -0,0 +1,40 @@
+//selecting DOM items
+
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuBranding = document.querySelector(".menu-branding");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".nav-item");
+
+//set intial state of menu
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach(item => item.classList.add("show"));
+    //set showMenu to true
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach(item => item.classList.remove("show"));
+    //set showMenu to true
+    showMenu = false;
+  }
+}
+
+var loveCount = parseInt(document.getElementById("lovecount").innerHTML);
+console.log(loveCount);
+
+function increase() {
+  loveCount = loveCount + 1;
+  document.getElementById("lovecount").innerHTML = loveCount;
+}
